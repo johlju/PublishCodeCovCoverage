6e803f53627a14f6cae3ec32e8a552797205e337
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-10-06 18:36:47 +0200

    fix: update type imports in Jest configuration files

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,4 +1,4 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('@jest/types').Config.InitialOptions} */
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
