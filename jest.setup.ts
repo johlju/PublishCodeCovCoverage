151ad5c9a556629b11869059183a9a4f13fd9d30
Johan Ljunggren <johlju@users.noreply.github.com>
2025-10-06 18:50:10 +0200

    chore: update dependencies and devDependencies in package.json (#87)

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,7 @@
 import { TextEncoder, TextDecoder } from 'util';
 
 // Mock TextEncoder and TextDecoder for Jest
-global.TextEncoder = TextEncoder;
+global.TextEncoder = TextEncoder as unknown as typeof global.TextEncoder;
 // Using type casting to avoid 'any'
 
 global.TextDecoder = TextDecoder as unknown as typeof global.TextDecoder;
